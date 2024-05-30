be63c84165f71a774ffd5633a4412b9e77bcfa7e
Styl1st <151127732+Styl1st@users.noreply.github.com>
2024-05-30 20:27:03 +0200

    adding Columns functions

--- Columns/Column.c
+++ Columns/Column.c
@@ -4,6 +4,7 @@
 
 #include "Column.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 COLUMN *create_column(char *title) {
     COLUMN *column = (COLUMN *) malloc(sizeof(COLUMN));
@@ -12,4 +13,26 @@ COLUMN *create_column(char *title) {
     column->logical_size = 0;
     column->data = (int *) malloc(sizeof(int) * column->physical_size);
     return column;
+}
+
+int insert_value(COLUMN *column, int value) {
+    if (column->logical_size == column->physical_size) {
+        column->physical_size *= 2;
+        column->data = (int *) realloc(column->data, sizeof(int) * column->physical_size);
+    }
+    column->data[column->logical_size++] = value;
+    return 0;
+}
+
+void delete_column(COLUMN **column) {
+    free((*column)->data);
+    free(*column);
+    *column = NULL;
+}
+
+void print_column(COLUMN *column) {
+    printf("Column: %s\n", column->title);
+    for (int i = 0; i < column->logical_size; i++) {
+        printf("%d\n", column->data[i]);
+    }
 }
--- Columns/Column.h
+++ Columns/Column.h
@@ -19,4 +19,24 @@ typedef struct {
  */
 COLUMN *create_column(char *title);
 
+/**
+ * Insert a value into the column
+ * @param column : pointer to column
+ * @param value : value to insert
+ * @return : 0 if success, -1 if error
+ */
+int insert_value(COLUMN *column, int value);
+
+/**
+ * Delete a column
+ * @param column : pointer to column
+ */
+void delete_column(COLUMN **column);
+
+/**
+ * Print the column
+ * @param column : pointer to column
+ */
+void print_column(COLUMN *column);
+
 #endif //UNTITLED_COLUMN_H
